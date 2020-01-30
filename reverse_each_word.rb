922c1de5be15d86a33ff06c32d87f0e1cc1f0a52
cerebral-caller-0985 <ashaffe@gmail.com>
2020-01-30 16:18:02 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,6 +2,7 @@ def reverse_each_word(inputstring)
   stringarray = inputstring.split(' ')
   stringarray.collect([]) do |emptarr, word|
     emptarr << word.reverse
+    emptarr
   end
   emptarray.join(' ')
 end
